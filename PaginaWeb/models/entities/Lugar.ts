3b79d0164566cb7988f3e0f204a757d19167a998
Bianka <biankasarayj@outlook.com>
2022-10-19 15:38:07 -0500

    Definir propiedades de lugar

--- PaginaWeb/models/entities/Lugar.ts
+++ PaginaWeb/models/entities/Lugar.ts
@@ -3,5 +3,19 @@ import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
 @Entity({ name: 'lugar' })
 export default class Lugares
 {
+    @PrimaryGeneratedColumn({ type: 'mediumint', unsigned: true})
+    public Id: number;
+
+    @Column({ type: "varchar", length: 30, nullable: false})
+    public nombre: string;
+
+    @Column({ type: "varchar", length: 150, nullable: false})
+    public descripcion: string;
+
+    @Column({ type: "varchar", length: 30, nullable: false})
+    public direccion: string;
+
+    @Column({ type: "varchar", length: 12})
+    public telefono: string;
 
 }
