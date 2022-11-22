986e5e114f7f9dbfd61706bde5258865175fa228
Bianka <biankasarayj@outlook.com>
2022-11-21 21:15:12 -0600

    Fix Archivos de ejecucion

---
+++ PaginaWeb/src/app.ts
@@ -0,0 +1,12 @@
+import express, {Application} from 'express';  
+import bodyParser from 'body-parser';  
+import AuthController from "../controllers/AuthController";  
+import LugaresController from '../controllers/LugaresController';  
+import ReservacionController from '../controllers/ReservacionController';
+  
+const app: Application = express();  
+app.use(bodyParser.json());  
+LugaresController.mount(app);  
+AuthController.mount(app);
+ReservacionController.mount(app);
+export default app;
